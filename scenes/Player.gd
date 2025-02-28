72f2a7c69abd2cfe2ac91ed62ec51d43a59f379f
Muhammad Raihan Akbar <ianakbar711@gmail.com>
2025-02-28 19:42:50 +0700

    fixed formatting

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -10,52 +10,53 @@ var is_crouching = false
 var can_dash = false
 var on_dash_cooldown = false
 var is_dashing = false
-var dash_direction = true #true for right and false for left
+var dash_direction = true  #true for right and false for left
 
 @onready var sprite = $Sprite2D
 @onready var dash_timer = $DashCheck
 @onready var dash_duration_timer = $DashDuration
 @onready var dash_cooldown_timer = $DashCooldown
 
+
 func _physics_process(delta: float) -> void:
 	_crouch()
-	
+
 	if is_on_floor():
 		can_double_jump = true
-	
+
 	velocity.y += delta * gravity
-	
-	if is_on_floor() and Input.is_action_just_pressed('ui_up'):
+
+	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
 		velocity.y = jump_speed
-	
-	if not is_on_floor() and can_double_jump and Input.is_action_just_pressed('ui_up'):
+
+	if not is_on_floor() and can_double_jump and Input.is_action_just_pressed("ui_up"):
 		velocity.y = 0.8 * jump_speed
 		can_double_jump = false
-	
+
 	if Input.is_action_just_released("ui_right") and not on_dash_cooldown:
 		dash_direction = true
 		can_dash = true
 		dash_timer.start()
-	
+
 	if Input.is_action_just_released("ui_left") and not on_dash_cooldown:
 		dash_direction = false
 		can_dash = true
 		dash_timer.start()
-	
+
 	if Input.is_action_just_pressed("ui_right") and can_dash and dash_direction:
 		velocity.x = dash_speed
 		is_dashing = true
 		can_dash = false
 		dash_duration_timer.start()
 		print("dash!")
-		
+
 	if Input.is_action_just_pressed("ui_left") and can_dash and not dash_direction:
 		velocity.x = -dash_speed
 		is_dashing = true
 		can_dash = false
 		dash_duration_timer.start()
 		print("dash!")
-	
+
 	if Input.is_action_pressed("ui_left") and not is_dashing:
 		sprite.flip_h = true
 		velocity.x = -walk_speed
@@ -64,8 +65,9 @@ func _physics_process(delta: float) -> void:
 		velocity.x = walk_speed
 	elif not is_dashing:
 		velocity.x = 0
-		
+
 	move_and_slide()
+
 
 func _crouch():
 	if Input.is_action_just_pressed("crouch"):
@@ -77,15 +79,18 @@ func _crouch():
 			walk_speed *= 0.5
 			sprite.frame = 3
 			is_crouching = true
-		
+
+
 func _on_dash_timeout() -> void:
 	can_dash = false
 	print("timeout!")
+
 
 func _on_dash_duration_timeout() -> void:
 	on_dash_cooldown = true
 	dash_cooldown_timer.start()
 	is_dashing = false
+
 
 func _on_dash_cooldown_timeout() -> void:
 	on_dash_cooldown = false
